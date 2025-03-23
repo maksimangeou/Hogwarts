da0ba28981eba7f6b3d34ee9f45f93c2c9c915a8
maksimangeou <ange_fuck@icloud.com>
2025-03-23 11:47:18 +0300

    1

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,17 +1,27 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.StudentService;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentController {
 
     private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
+    }
+
+    @GetMapping("{id}")
+    public Student getStudent(@PathVariable long id) {
+        return studentService.findStudent(id);
+    }
+
+    @PostMapping
+    public Student postStudent(Student student) {
+        return studentService.createStudent(student);
     }
 
 

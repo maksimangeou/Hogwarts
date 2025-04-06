52a994c74e026cccee84cb100d5bafd1265bdfa1
maksimangeou <ange_fuck@icloud.com>
2025-04-06 19:58:26 +0300

    3

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,10 @@ import ru.hogwarts.school.service.InfoService;
 @RequestMapping("/info-port")
 public class InfoController {
 
+
     @Autowired
     private final InfoService infoService;
 
-    @Autowired
     public InfoController(InfoService infoService) {
         this.infoService = infoService;
     }
--- src/main/java/ru/hogwarts/school/service/InfoService.java
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -2,11 +2,24 @@ package ru.hogwarts.school.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Info;
 
-public interface InfoService {
-
-    Info getPort();
+@Service
+public class InfoService {
 
     Logger logger = LoggerFactory.getLogger(InfoService.class);
+
+
+    @Value("${server.port}")
+    private int port;
+
+    public Info getPort() {
+        logger.info("Запуск метода getPort в portFirstType");
+        Info info = new Info();
+        info.setPort(port);
+        logger.debug("portFirstType = {}", info);
+        return info;
+    }
 }
